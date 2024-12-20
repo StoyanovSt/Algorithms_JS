9b191787f8fa892d64473e6ea3b9131295469b71
Stiliyan <stiliqnskiq.st@gmail.com>
2024-12-20 14:19:41 +0200

    recursive factorial

--- factorial.js
+++ factorial.js
@@ -9,6 +9,12 @@ function factorial(n) {
 
   return result;
 }
+
+//O(n)
+function factorialRecursive(n) {
+  if (n === 0) return 1;
+  return n * factorialRecursive(n - 1);
+}
 console.log(factorial(0));
 console.log(factorial(1));
 console.log(factorial(5));
